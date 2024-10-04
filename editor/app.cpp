ac472c4ef06dae8cb88ec2ec550899501634e32b
daonvshu <1286772338@qq.com>
2024-10-04 15:40:47 +0800

    add editor system button

--- editor/app.cpp
+++ editor/app.cpp
@@ -1,6 +1,8 @@
 #include "app.h"
 
 #include <QWKWidgets/widgetwindowagent.h>
+#include <qdebug.h>
+#include <qtimer.h>
 
 App::App(QWidget *parent)
     : QWidget(parent)
@@ -10,4 +12,29 @@ App::App(QWidget *parent)
     auto agent = new QWK::WidgetWindowAgent(this);
     agent->setup(this);
     agent->setTitleBar(ui.app_title);
+    agent->setHitTestVisible(ui.system_buttons);
+}
+
+void App::on_btn_min_clicked() {
+    showMinimized();
+}
+
+void App::on_btn_max_clicked() {
+    if (isMaximized()) {
+        showNormal();
+    } else {
+        showMaximized();
+    }
+}
+
+void App::on_btn_close_clicked() {
+    close();
+}
+
+void App::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+
+    QTimer::singleShot(0, this, [=]() {
+        ui.btn_max->setIcon(isMaximized() ? QIcon(":/res/maxsize2.svg") : QIcon(":/res/maxsize1.svg"));
+    });
 }
--- editor/app.h
+++ editor/app.h
@@ -5,9 +5,19 @@
 #include "ui_app.h"
 
 class App : public QWidget {
+    Q_OBJECT
+
 public:
     explicit App(QWidget *parent = nullptr);
 
 private:
     Ui::App ui;
+
+private slots:
+    void on_btn_min_clicked();
+    void on_btn_max_clicked();
+    void on_btn_close_clicked();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
 };
